0900f3fea31e18a72f671dac083ed6f6d2925072
irasan <iryna.sanzhara@gmail.com>
2021-08-11 11:37:04 +0000

    Update context processor and add display of bag items quantity in the header

--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
-# Create your views here.
 
+# Create your views here.
 def view_bag(request):
     """ A view that renders the bag contents page """
 

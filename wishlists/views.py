c9bef24287b5f38bfeab3e1b583aa30279fb0e3b
irasan <iryna.sanzhara@gmail.com>
2021-08-29 05:06:14 +0000

    Add view and button for saving products to wishlist

--- wishlists/views.py
+++ wishlists/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def view_wishlist(request):
     """
-    View to display wishlist
+    View to display wishlist on a separate page
     """
     user = get_object_or_404(UserProfile, user=request.user)
     wishlist = user.wishlist.all()
@@ -24,3 +24,30 @@ def view_wishlist(request):
     }
     return render(request, template, context)
 
+
+@login_required
+def add_to_wishlist(request, product_id):
+    """
+    View to add products to wishlist
+    """
+    user = get_object_or_404(UserProfile, user=request.user)
+    product = get_object_or_404(Product, pk=product_id)
+
+    wishlist = Wishlist.objects.create(user=user)
+
+    wishlist_item = WishlistItem.objects.create(wishlist=wishlist,
+                                                product=product)
+    wishlist_item.save()
+
+    messages.success(
+        request, f'Added {product.name} to your wishlist')
+
+    return redirect(reverse('view_wishlist'))
+
+
+@login_required
+def remove_from_wishlist(request, product_id):
+    """
+    View to add products to wishlist
+    """
+    return redirect(reverse('view_wishlist'))

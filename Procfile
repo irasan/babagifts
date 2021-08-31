e56eb387e6857aa5bdf50f857ba2ec57b65a7b23
irasan <iryna.sanzhara@gmail.com>
2021-08-31 11:17:42 +0000

    Add procfile, host name and prepare for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn babagifts.wsgi:application
--- babagifts/settings.py
+++ babagifts/settings.py
@@ -27,11 +27,10 @@ SECRET_KEY = 'django-insecure-xn3x(^*40if(gh8)azs@b_or!%d+0+ns*ga$kys-vy5eioup#7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['babagifts.herokuapp.com', 'localhost']
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -122,12 +121,17 @@ WSGI_APPLICATION = 'babagifts.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
